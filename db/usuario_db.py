8fdc7e9e661b356831f34f17f3a031171848d67b
Diego Vega <davegav@unal.edu.co>
2020-12-12 16:01:00 -0500

    error 2

--- db/usuario_db.py
+++ db/usuario_db.py
@@ -1,16 +1,16 @@
 from typing import Dict
 from pydantic import BaseModel
 
-class usuarioInDB(BaseModel):
+class UsuarioInDB(BaseModel):
     nombre: str   # usuarioInDB.nombre = "diego"
     mejorpuntaje: int
     ultimopuntaje: int
 
-database_usuarios = Dict[str, usuarioInDB] # {"nombre": "diego", }
-database_usuarios = {"Diego": usuarioInDB(**{"nombre":"Diego",
+database_usuarios = Dict[str, UsuarioInDB] # {"nombre": "diego", }
+database_usuarios = {"Diego": UsuarioInDB(**{"nombre":"Diego",
                 "mejorpuntaje":1000,
                 "ultimopuntaje":10}),
-                "Felix": usuarioInDB(**{"nombre":"Felix",
+                "Felix": UsuarioInDB(**{"nombre":"Felix",
                 "mejorpuntaje":30,
                 "ultimopuntaje":30})}
 def get_usuario(nombre: str):
@@ -18,6 +18,6 @@ def get_usuario(nombre: str):
         return database_usuarios[nombre]
     else:
         return None
-def update_usuario(usuario_in_db: usuarioInDB):
+def update_usuario(usuario_in_db: UsuarioInDB):
     database_usuarios[usuario_in_db.nombre] = usuario_in_db
     return usuario_in_db
